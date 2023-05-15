db85f039afe95a4cd664217dc8757ac6d698342a
Zorous <oussamabelhadi5@gmail.com>
2023-05-15 23:49:07 +0200

    server updates

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.use(cors());
 app.use(express.json());
 
 const Tools = require("./models/Tool");
+const ToolsComments = require("./models/ToolComment");
 
 const uri =
   "mongodb+srv://techbible:nRgcJ2M8O6DRoznj@techbible.eggj9te.mongodb.net/techbible";
@@ -22,17 +23,39 @@ app.get("/mongo-tools", async (req, res) => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("Connected to MongoDB");
+    // console.log("Connected to MongoDB");
     const tools = await Tools.find();
 
-    console.log("TOOLS : ",tools);
+    // console.log("TOOLS : ",tools);
     res.send(tools); // Send an object containing both variables
-
-
   } catch (error) {
     console.error(error);
     res.status(500).send("Error fetching tools data");
   }
+});
+
+// To GET TOOL COMMENTS
+app.get("/mongo-toolComments/:toolId", async (req, res) => {
+  let { toolId } = req.params;
+
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    const toolsComments = await ToolsComments.find({ toolId: toolId });
+
+    res.send(toolsComments);
+    console.log("toolsComments :" + toolsComments);
+    console.log(typeof toolsComments);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching tools data");
+  }
+  // finally {
+  //   mongoose.connection.close();
+  // }
 });
 
 // app.post("/like/:id/:uid", async (req, res) => {
@@ -46,8 +69,6 @@ app.get("/mongo-tools", async (req, res) => {
 //   }
 //   console.log("tool has been liked successfully!!!!!");
 // });
-
-
 
 app.post("/like/:id/:uid", async (req, res) => {
   let { id, uid } = req.params;
@@ -82,16 +103,71 @@ app.post("/unlike/:id/:uid", async (req, res) => {
   console.log("tool has been unliked succefuly!!!!!");
 });
 
+// Add a Tool Comment
+app.post("/addToolComment/:toolId/:userId/:commentText", async (req, res) => {
+  try {
+    const { toolId, userId, commentText } = req.params;
+    const newComment = await ToolsComments.create({
+      text: commentText,
+      userId: userId,
+      toolId: toolId,
+    });
+    // assuming that `Tools` is the model for the tools collection
+    const tool = await Tools.findById(toolId);
+    tool.comments.push(newComment._id);
+    await tool.save();
+    res.status(201).json(newComment);
+    console.log("comment added");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error adding tool comment");
+  }
+});
 
+// LIKE A TOOL COMMENT
+app.post("/likeToolComment/:toolCommentId/:userId", async (req, res) => {
+  let { toolCommentId, userId } = req.params;
+  try {
+    await ToolsComments.findByIdAndUpdate(toolCommentId, {
+      $push: { likedBy: userId },
+    });
+    console.log("Tool comment has been liked successfully!!!!!");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// UNLIKE A TOOL COMMENT
+app.post("/unlikeToolComment/:toolCommentId/:userId", async (req, res) => {
+  let { toolCommentId, userId } = req.params;
+  try {
+    const toolComment = await ToolsComments.findById(toolCommentId);
+    // Remove the uid from the LikedBy array using the filter method
+    const updatedLikedBy = toolComment.likedBy?.filter(
+      (likedByUid) => likedByUid !== userId
+    );
+    // Update the tool document with the updated LikedBy array
+    const updatedToolComment = await ToolsComments.findByIdAndUpdate(
+      toolCommentId,
+      {
+        likedBy: updatedLikedBy,
+      }
+    );
+
+    console.log("tool Comment has been unliked succefuly!!!!!");
+    return res.send(updatedToolComment);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.post("", async (req, res) => {
   try {
-    const tools = await Tools
-      .find({ Category: { $in: res.interests } })
+    const tools = await Tools.find({ Category: { $in: res.interests } })
       .limit(3)
       .toArray();
 
-    console.log('Tools:', tools);
+    console.log("Tools:", tools);
     // Do something with the tools array
 
     // Close the MongoDB connection when finished
@@ -99,10 +175,6 @@ app.post("", async (req, res) => {
     console.log(error);
   }
 });
-
-
-
-
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
